bbbd8b69ceff65e6ec7f093db7e6847c3976a421
ANDREWW <olisaemekaandrew1@gmail.com>
2025-07-12 00:47:00 +0100

    Added initial project files for a weather app, including HTML structure, CSS styling, JavaScript functionality for API calls, and images for weather icons. Also created a .gitignore file to exclude unnecessary files from version control.

---
+++ script.js
@@ -0,0 +1,55 @@
+// lets create the function to call the weather api
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+const API_KEY = " "; //"your-api-key-here";
+const API_URL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function getWeather(city) {
+  const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
+
+  // to make sure the error message is only displayed when city id not found.
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+    return;
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/raining.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  getWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getWeather(searchBox.value);
+  }
+});
+
+getWeather();
